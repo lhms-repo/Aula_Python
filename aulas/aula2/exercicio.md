570d9060e6e1fabeee400d93209b743ed742f85f
L <aasdagrsghdththj@gmail.com>
2022-01-29 22:04:20 -0300

    Adiciona exercicio proposto e coloca novos casos condicionais na aula 2

---
+++ aulas/aula2/exercicio.md
@@ -0,0 +1,7 @@
+# Exercício da aula 2
+## Proposta
+Crie um script Python que leia (input) o nome (string), se a pessoa é casada ou não (bool) e a idade (int) de 2 pessoas, mostrando no terminal (print) o resultado da comparação entre cada um dos valores. Espera-se uma saída semelhante à mostrada abaixo:
+
+Os nomes são iguais? \<True ou False><br>
+A primeira pessoa é mais velha que a segunda? \<True ou False><br>
+Alguma das pessoas são casadas? \<True ou False>
--- aulas/aula2/operacoes_condicionais.py
+++ aulas/aula2/operacoes_condicionais.py
@@ -19,3 +19,14 @@ print('\na é maior que b?\n' + str( a >= b ) )
 
 # Operação com menor ou igual ( <= )
 print('\na é maior que b?\n' + str( a <= b ) )
+
+# Operação com conta
+print('\n(a-b+1) é maior ou igual a (-a*b)?\n' + str( a - b + 1 >= -a * b ) )
+
+# Operação composta
+print('\n(a-b+1) é maior ou igual a (-a*b) ou (-b * a < 6)?\n' + str(   (a - b + 1 >= -a * b ) or (-b * a < 6)  ) )
+
+# Comparação com strings
+a = 'palavra'
+b = 'palavra'
+print('\na é igual a b?\n' + str( a == b ) )
